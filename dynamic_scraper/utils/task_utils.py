6f1aa38484a0d92edd3aab10b0d249d2f263c568
Scott Coates <scoarescoare@gmail.com>
2013-12-13 16:50:41 -0500

    Revert "Added a lock around each spider"
    This reverts commit 27cba23801c63f1c1207be8c9da054a4c7e3f4a7.

--- dynamic_scraper/utils/task_utils.py
+++ dynamic_scraper/utils/task_utils.py
@@ -6,10 +6,6 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 settings = get_project_settings()
 from dynamic_scraper.models import Scraper
-from django.core.cache import cache
-import logging
-
-logger = logging.getLogger(__name__)
 
 class TaskUtils():
     
@@ -118,32 +114,18 @@ class ProcessBasedUtils(TaskUtils):
         log.msg('Spider stopped.')
         crawler.stop()
 
-
     def _run_spider(self, **kwargs):
-      # the reason we're checking here and not `pending_jobs` is because this gives more useful info to make the
-      # decision
+        param_dict = {
+            'project': 'default',
+            'spider': kwargs['spider'],
+            'id': kwargs['id'],
+            'run_type': kwargs['run_type'],
+            'do_action': kwargs['do_action']
+        }
 
-      cache_key = "{0}-lock-{1}".format(kwargs['spider'], kwargs['id'])
-
-      if cache.add(cache_key, True):
-        try:
-          param_dict = {
-              'project': 'default',
-              'spider': kwargs['spider'],
-              'id': kwargs['id'],
-              'run_type': kwargs['run_type'],
-              'do_action': kwargs['do_action']
-          }
-
-          p = Process(target=self._run_crawl_process, kwargs=param_dict)
-          p.start()
-          p.join()
-
-        finally:
-          cache.delete(cache_key)
-
-      else:
-        logger.info("Spider not started. {0} is alreadying running".format(cache_key))
+        p = Process(target=self._run_crawl_process, kwargs=param_dict)
+        p.start()
+        p.join()
 
     def _pending_jobs(self, spider):
         # don't worry about scheduling new jobs if there are still pending jobs for same spider
